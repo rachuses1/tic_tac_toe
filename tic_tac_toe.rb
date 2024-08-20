7584f77378f3ca32d6d3a3c9aa395c9ffc874d5f
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-20 19:02:45 +1000

    added winning conditions

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,7 +7,7 @@
 
 # Prompt for
 # Player One's turn: 
-# replace "__" with X
+# replace "__" with "X "
 
 # Prompt for
 # Player Two's turn:
@@ -35,7 +35,8 @@ C2 = "__"
 C3 = "__"
 
 def board()
-  puts   "    A  B  C
+  puts   "
+    A  B  C
   1 #{A1}|#{B1}|#{C1}
   2 #{A2}|#{B2}|#{C2}
   3 #{A3}|#{B3}|#{C3}"
@@ -222,6 +223,45 @@ def update_player_two(choice)
 
 end
 
+def win()
+  # Winning Conditions
+  #   columns (all A, all B, all C)
+  #   rows (all 1, all 2, all 3)
+  #   diagonal (A1 B2 C3, C1 B2 A3)
+
+  # Player One Wins
+  if 
+    A1 == "X " && A2 == "X " && A3 == "X " ||
+    B1 == "X " && B2 == "X " && B3 == "X " ||
+    C1 == "X " && C2 == "X " && C3 == "X " ||
+
+    A1 == "X " && B1 == "X " && C1 == "X " ||
+    A2 == "X " && B2 == "X " && C2 == "X " ||
+    A3 == "X " && B3 == "X " && C3 == "X " ||
+
+    A1 == "X " && B2 == "X " && C3 == "X " ||
+    C1 == "X " && B2 == "X " && A3 == "X "
+
+    abort("Player One Wins")
+    
+  end
+
+  # Player Two wins
+  if 
+    A1 == "O " && A2 == "O " && A3 == "O " ||
+    B1 == "O " && B2 == "O " && B3 == "O " ||
+    C1 == "O " && C2 == "O " && C3 == "O " ||
+
+    A1 == "O " && B1 == "O " && C1 == "O " ||
+    A2 == "O " && B2 == "O " && C2 == "O " ||
+    A3 == "O " && B3 == "O " && C3 == "O " ||
+
+    A1 == "O " && B2 == "O " && C3 == "O " ||
+    C1 == "O " && B2 == "O " && A3 == "O "
+
+    abort("Player Two Wins")
+  end
+end
 
 class PlayerTurn
   def initialize()
@@ -229,10 +269,10 @@ class PlayerTurn
     @fill = 0
   end
 
-  def display()
-    puts "Turn is #{@turn}"
-    puts "Fill is #{@fill}"
-  end
+  # def display()
+  #   puts "Turn is #{@turn}"
+  #   puts "Fill is #{@fill}"
+  # end
 
   def player_one()
     board()
@@ -241,7 +281,6 @@ class PlayerTurn
     
     @turn += 1
     @fill += 1
-    puts "Player One's choice is #{player_one_choice}"
     update_player_one(player_one_choice)
     display()
   end
@@ -254,7 +293,6 @@ class PlayerTurn
     update_player_two(player_two_choice)
     @turn = 0
     @fill += 1
-    puts "Player Two's choice is #{player_two_choice}"
     display()
   end
 
@@ -262,8 +300,10 @@ class PlayerTurn
     while @fill < 9
       if @turn == 0
         player_one()
+        win()
       else
         player_two()
+        win()
       end
     end
   end
